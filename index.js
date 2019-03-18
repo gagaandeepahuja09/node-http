801f8677ad191c48ff701d5f967754f5584c5675
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-18 14:14:47 +0530

    node-file system and path module

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const http = require('http');
-
+// Using Node File System module
+const fs = require('fs');
+const path = require('path');
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-	console.log(req.headers);
+	console.log(`Request for ${req.url} by method ${req.method}`);
+	if (req.method == 'GET') {
+		let fileUrl = '/index.html';
+		if(req.url != '/') {
+			fileUrl = req.url;
+		}
+		// Use Node File System Path Module
+		let filePath = path.resolve(`./public${fileUrl}`);
+		const fileExt = path.extname(filePath);
+		if(fileExt == '.html') {
+			// Check if file exists using callback function
+			fs.exists(filePath, (exists) => {
+				if(!exists) {
+					res.statusCode = 404;
+					res.setHeader('Content-type', 'header');
+					res.end(`<html><body><h1>Error: ${fs.filePath} not
+					found</h1></body></html>`);
+				}
+			});
+		}
+	}
+	else {
+	}
+
+	/*console.log(req.headers);
 	res.statusCode = 200;
 	res.setHeader('Content-type', 'text/html');
-	res.end('<html><body><h1>Hey</h1></body></html>');
+	res.end('<html><body><h1>Hey</h1></body></html>');*/
 });
 
 server.listen(port, hostname, () => {
